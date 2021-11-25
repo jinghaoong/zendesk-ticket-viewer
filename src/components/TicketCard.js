2ae0344286673e4dab4bd31936e38d0315ee2322
Jing Hao Ong <jinghaoong@gmail.com>
2021-11-25 15:30:56 +0800

    Updated Ticket Card, used in TicketViewer list view

--- src/components/TicketCard.js
+++ src/components/TicketCard.js
@@ -1,18 +1,47 @@
 import {
   Card,
+  CardActions,
   CardContent,
+  Chip, IconButton,
+  Stack,
   Typography
 } from '@mui/material';
+import { FiMoreHorizontal } from 'react-icons/fi';
 
-const TicketCard = ({ ticket }) => {
+const statusCodes = {
+  'open': 'o',
+  'pending': 'p',
+  'solved': 's',
+  'closed': 'c'
+};
+
+const TicketCard = ({ handleClickOpen, ticket }) => {
   return (
-    <Card back variant="outlined">
+    <Card key={ticket.id} variant="outlined" sx={{ my: 1 }}>
       <CardContent>
-        <Typography variant="h5">{ticket.subject}</Typography>
-        <Typography variant="h7">{ticket.description}</Typography>
+        <Stack direction="row" spacing={2}>
+          <Typography variant="subtitle2">#{ticket.id}</Typography>
+          <Typography>{ticket.subject}</Typography>
+        </Stack>
+        <Stack>
+          <Typography>Requester id: <b>{ticket.requester_id}</b></Typography>
+          <Typography>Assignee id: <b>{ticket.assignee_id}</b></Typography>
+        </Stack>
       </CardContent>
+      <CardActions>
+        <Stack direction="row" spacing={1}>
+          <Chip label={ticket.status} />
+          <IconButton
+            size="small"
+            title="View Details"
+            onClick={() => handleClickOpen(ticket.url)}
+          >
+            <FiMoreHorizontal />
+          </IconButton>
+        </Stack>
+      </CardActions>
     </Card>
   );
-}
+};
 
 export default TicketCard;
