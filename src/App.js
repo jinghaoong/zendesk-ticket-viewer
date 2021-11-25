63e00ee797134af0f87abf9c87d91892d2ea1b35
Jing Hao Ong <jinghaoong@gmail.com>
2021-11-25 15:29:36 +0800

    added Header to webpage

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import TicketViewer from './components/TicketViewer';
 import { StyledEngineProvider } from '@mui/styled-engine';
+import Header from './components/Header';
 
 function App() {
   return (
     <StyledEngineProvider injectFirst>
+      <Header />
       <TicketViewer />
     </StyledEngineProvider>
   );
