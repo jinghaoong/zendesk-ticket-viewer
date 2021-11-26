9b8e86b35f16ef60c1467e60b7b444bb33ec3797
Jing Hao Ong <jinghaoong@gmail.com>
2021-11-27 02:32:03 +0800

    Updated to display actual error message

--- src/components/Error.js
+++ src/components/Error.js
@@ -4,8 +4,8 @@ import { Alert, AlertTitle } from '@mui/material';
 const Error = ({ error }) => {
   return (
     <Alert severity="warning">
-      <AlertTitle>Error</AlertTitle>
-      Error Message
+      <AlertTitle>Something Went Wrong!</AlertTitle>
+      {error}
     </Alert>
   )
 }
