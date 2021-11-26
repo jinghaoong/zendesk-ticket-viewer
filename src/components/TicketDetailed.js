1bfd95492ad41c0fb8420e7d819a75d0ed8a01bc
Jing Hao Ong <jinghaoong@gmail.com>
2021-11-27 02:33:45 +0800

    TicketDetailed view now shows requester/assignee names and relevant dates

--- src/components/TicketDetailed.js
+++ src/components/TicketDetailed.js
@@ -1,10 +1,36 @@
-import { Button, Dialog, DialogActions, DialogContent, DialogTitle, Skeleton, Stack, Typography } from "@mui/material";
-import { useEffect, useState } from "react";
+import { Button, Dialog, DialogActions, DialogContent, DialogTitle, Skeleton, Stack, Typography } from '@mui/material';
+import { useEffect, useState } from 'react';
+import { parseISO, formatDistanceToNow, format } from 'date-fns';
+import Error from './Error';
+
+const auth = `Bearer ${process.env.REACT_APP_TOKEN}`;
+
+const convertDate = (isoDate) => {
+  return format(parseISO(isoDate), 'eeee, dd MMMM yyyy');
+}
+
+const convertDateToNow = (isoDate) => {
+  return formatDistanceToNow(parseISO(isoDate), { addSuffix: true });
+}
 
 const TicketDetailed = ({ open, handleClose, ticketUrl }) => {
-  const [error, setError] = useState(false);
+  const [error, setError] = useState();
   const [ticket, setTicket] = useState();
-  const auth = `Bearer ${process.env.REACT_APP_TOKEN}`;
+  const [users, setUsers] = useState([]);
+
+  const fetchUser = async (user_id) => {
+    return fetch(
+      `${process.env.REACT_APP_ZCC_URL}api/v2/users/${user_id}`, {
+      method: 'GET',
+      mode: 'cors',
+      credentials: 'same-origin',
+      headers: {
+        'Authorization': auth
+      }
+    })
+      .then(response => response.json())
+      .then(data => data.user);
+  }
 
   const fetchTicket = async () => {
     fetch(ticketUrl, {
@@ -18,9 +44,18 @@ const TicketDetailed = ({ open, handleClose, ticketUrl }) => {
       .then(response => response.json())
       .then(data => {
         setTicket(data.ticket);
+        return data.ticket;
+      })
+      .then(ticket => {
+        const fetchRequester = fetchUser(ticket.requester_id);
+        const fetchAssignee = fetchUser(ticket.assignee_id);
+
+        Promise.all([fetchRequester, fetchAssignee])
+          .then(data => setUsers(data));
       })
       .catch(error => {
         console.error(error);
+        setError(error);
       })
   }
 
@@ -32,34 +67,47 @@ const TicketDetailed = ({ open, handleClose, ticketUrl }) => {
     console.log(ticket);
   }, [ticket]);
 
+  useEffect(() => {
+    console.log(users);
+  }, [users]);
+
   return (
     <Dialog open={open} onClose={handleClose} fullWidth>
       <DialogTitle>
-        <Typography variant="h5">
+        <Typography>
           {ticket && `#${ticket.id} -- ${ticket.subject}`}
-          {!ticket && <Skeleton />}
+          {(!ticket && !error) && <Skeleton />}
         </Typography>
       </DialogTitle>
       <DialogContent>
+        {error && <Error error={error} />}
         <Stack spacing={1}>
           <Typography variant="body1">
             {ticket && ticket.description}
-            {!ticket && <Skeleton />}
+            {(!ticket && !error) && <Skeleton />}
           </Typography>
-          <Typography variant="body2">
-            {ticket && `Requester: ${ticket.requester_id}`}
-            {!ticket && <Skeleton />}
+          <Typography>
+            {ticket && `Created: ${convertDate(ticket.created_at)}`}
+            {(!ticket && !error) && <Skeleton width={200} />}
           </Typography>
-          <Typography variant="body2">
-            {ticket && `Assignee: ${ticket.assignee_id}`}
-            {!ticket && <Skeleton />}
+          <Typography>
+            {ticket && `Last updated ${convertDateToNow(ticket.updated_at)}`}
+            {(!ticket && !error) && <Skeleton width={200} />}
+          </Typography>
+          <Typography>
+            {users[0] && `Requester: ${users[0].name}`}
+            {(!users[0] && !error) && <Skeleton width={200} />}
+          </Typography>
+          <Typography>
+            {users[1] && `Assignee: ${users[1].name}`}
+            {(!users[1] && !error) && <Skeleton width={200} />}
           </Typography>
         </Stack>
       </DialogContent>
       <DialogActions>
-        <Button onClick={handleClose}>Close</Button>
+        <Button variant="contained" onClick={handleClose}>Close</Button>
       </DialogActions>
-    </Dialog>
+    </Dialog >
   );
 };
 
