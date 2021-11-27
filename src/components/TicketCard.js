87723cf7df41012afa5824de14ffc7d78ea31395
Jing Hao Ong <jinghaoong@gmail.com>
2021-11-27 15:18:22 +0800

    Implemented breakpoint responsiveness to show details in larger screens and updated theming

--- src/components/TicketCard.js
+++ src/components/TicketCard.js
@@ -1,19 +1,38 @@
+import { useTheme } from '@emotion/react';
 import {
   Card,
   CardActions,
   CardContent,
+  CardHeader,
   Chip,
+  Divider,
   IconButton,
   Skeleton,
   Stack,
-  Typography
+  Typography,
+  useMediaQuery
 } from '@mui/material';
-import { FiMoreHorizontal } from 'react-icons/fi';
+import { format, formatDistanceToNow, parseISO } from 'date-fns';
 import { useEffect, useState } from 'react';
+import { FiMoreHorizontal } from 'react-icons/fi';
 
 const auth = `Bearer ${process.env.REACT_APP_TOKEN}`;
 
+const convertDate = (isoDate) => {
+  return format(parseISO(isoDate), 'eee, dd MMM yyyy');
+};
+
+const convertDateToNow = (isoDate) => {
+  return formatDistanceToNow(parseISO(isoDate), { addSuffix: true });
+};
+
+const capitaliseString = (string) => {
+  return string[0].toUpperCase() + string.substring(1);
+};
+
 const TicketCard = ({ handleClickOpen, ticket }) => {
+  const theme = useTheme()
+  const showDetails = useMediaQuery(theme.breakpoints.up(768));
   const [users, setUsers] = useState([]);
 
   const fetchUser = async (user_id) => {
@@ -28,36 +47,103 @@ const TicketCard = ({ handleClickOpen, ticket }) => {
     })
       .then(response => response.json())
       .then(data => data.user);
-  }
+  };
 
   const fetchUsers = async () => {
     const fetchRequester = fetchUser(ticket.requester_id);
     const fetchAssignee = fetchUser(ticket.assignee_id);
 
     Promise.all([fetchRequester, fetchAssignee]).then(data => setUsers(data));
-  }
+  };
 
   useEffect(() => {
     fetchUsers();
-  }, [])
+  }, []);
 
   return (
     <Card key={ticket.id} variant="outlined" sx={{ my: 1 }}>
+      <CardHeader
+        title={
+          !(users[0] && users[1])
+            ?
+            <Skeleton width={200} />
+            :
+            <>
+              <Stack direction="row" spacing={1} alignItems="center">
+                <Chip size="small" color={ticket.status} label={ticket.status[0]} />
+                {showDetails ?
+                  <Typography color="gray">
+                    {ticket.type === null ? 'Ticket'
+                      : capitaliseString(ticket.type)}
+                    {` #${ticket.id}`}
+                  </Typography>
+                  :
+                  <Typography color="gray">#{ticket.id}</Typography>
+                }
+
+                {showDetails && <Typography variant="h6">{ticket.subject}</Typography>}
+              </Stack>
+              {!showDetails && <Typography variant="h6">{ticket.subject}</Typography>}
+            </>
+        }
+      />
       <CardContent>
-        <Stack direction="row" spacing={2}>
-          <Typography variant="subtitle2">#{ticket.id}</Typography>
-          <Typography>{ticket.subject}</Typography>
-        </Stack>
-        <Stack>
-          {users[0] && <Typography>Requester: <b>{users[0].name}</b></Typography>}
-          {!users[0] && <Skeleton width={200} />}
-          {users[1] && <Typography>Assignee: <b>{users[1].name}</b></Typography>}
-          {!users[1] && <Skeleton width={200} />}
+        <Stack
+          direction="row"
+          spacing={3}
+        >
+          {showDetails &&
+            <Typography width='60%'>
+              {ticket.description}
+            </Typography>
+          }
+          <Stack spacing={1}>
+            {showDetails &&
+              <>
+                {ticket &&
+                  <Typography>
+                    Created:
+                    &nbsp;
+                    {convertDate(ticket.created_at)}
+                    &nbsp;
+                    ({convertDateToNow(ticket.created_at)})
+                  </Typography>
+                }
+                {!ticket && <Skeleton width={200} />}
+                {ticket &&
+                  <Typography>
+                    Updated:
+                    &nbsp;
+                    {convertDate(ticket.updated_at)}
+                    &nbsp;
+                    ({convertDateToNow(ticket.updated_at)})
+                  </Typography>
+                }
+                {!ticket && <Skeleton width={200} />}
+                <Divider />
+              </>
+            }
+            {users[0] &&
+              <Typography>
+                Requester:
+                &nbsp;
+                <b>{users[0].name}</b>
+              </Typography>
+            }
+            {!users[0] && <Skeleton width={200} />}
+            {users[1] &&
+              <Typography>
+                Assignee:
+                &nbsp;&nbsp;
+                <b>{users[1].name}</b>
+              </Typography>
+            }
+            {!users[1] && <Skeleton width={200} />}
+          </Stack>
         </Stack>
       </CardContent>
-      <CardActions>
-        <Stack direction="row" spacing={1}>
-          <Chip label={ticket.status} />
+      {!showDetails &&
+        <CardActions>
           <IconButton
             size="small"
             title="View Details"
@@ -65,9 +151,9 @@ const TicketCard = ({ handleClickOpen, ticket }) => {
           >
             <FiMoreHorizontal />
           </IconButton>
-        </Stack>
-      </CardActions>
-    </Card>
+        </CardActions>
+      }
+    </Card >
   );
 };
 
