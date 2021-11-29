6412571c943dacbae2e2961332fcf5d3cccc5e6b
Jing Hao Ong <jinghaoong@gmail.com>
2021-11-29 19:40:40 +0800

    Updated App.js test file

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,41 @@
-import { render, screen } from '@testing-library/react';
+import { ThemeProvider } from '@emotion/react';
+import { StyledEngineProvider } from '@mui/styled-engine';
+import { mount, shallow } from 'enzyme';
 import App from './App';
+import theme from './theme';
+import Header from './components/Header/Header';
+import TicketViewer from './components/Ticket/TicketViewer';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+const themeMui = theme;
+const wrapper = mount(<App />);
+
+describe('App', () => {
+  it('Renders without crashing', () => {
+    shallow(<App />);
+  });
+
+  it('StyledEngineProvider is rendered', () => {
+    const styledEngineProviderMui = wrapper.find(StyledEngineProvider);
+    expect(styledEngineProviderMui).toBeDefined();
+  });
+
+  it('ThemeProvider is rendered', () => {
+    const themeProviderMui = wrapper.find(ThemeProvider);
+    expect(themeProviderMui).toBeDefined();
+  });
+
+  it('ThemeProvider accepts correct theme props', () => {
+    const themeProviderMui = wrapper.find(ThemeProvider);
+    expect(themeProviderMui.props().theme).toEqual(themeMui);
+  });
+
+  it('Header is rendered', () => {
+    const header = wrapper.find(Header);
+    expect(header).toBeDefined();
+  });
+
+  it('TicketViewer is rendered', () => {
+    const ticketViewer = wrapper.find(TicketViewer);
+    expect(ticketViewer).toBeDefined();
+  });
 });
