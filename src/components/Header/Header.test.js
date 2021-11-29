09034c45bb0bda36ab1f0367937326bba15c1066
Jing Hao Ong <jinghaoong@gmail.com>
2021-11-29 19:40:58 +0800

    Added Header.js test file

---
+++ src/components/Header/Header.test.js
@@ -0,0 +1,23 @@
+import { Typography } from '@mui/material';
+import { Box } from '@mui/system';
+import { mount } from 'enzyme';
+import Header from "./Header";
+
+const wrapper = mount(<Header />);
+
+describe('Header', () => {
+  it('Renders wrapping Box MUI component', () => {
+    const boxMui = wrapper.find(Box);
+    expect(boxMui).toBeDefined();
+  });
+
+  it('Renders Typography MUI component', () => {
+    const typographyMui = wrapper.find(Typography);
+    expect(typographyMui).toBeDefined();
+  });
+
+  it('Renders "Zendesk Ticket Viewer" as header', () => {
+    const header = wrapper.find(Typography).text();
+    expect(header).toEqual('Zendesk Ticket Viewer');
+  });
+})
